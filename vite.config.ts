134c7ba5442f2bb6fc79950c5cf3be5a17f74ffe
eullon1234-creator <eullon1234@gmail.com>
2026-09-02 17:20:15 -0300

    21

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Garante que caminhos de CSS/JS funcionem perfeitamente no GitHub Pages
 })
